fcfee22180de2ba9f5f2f6924ec248f8e0965cf2
Starforcraft <starforcraft@yahoo.com>
2021-05-31 17:26:31 +0200

    Update AllTheModium.java

--- src/main/java/com/thevortex/allthemodium/AllTheModium.java
+++ src/main/java/com/thevortex/allthemodium/AllTheModium.java
@@ -57,6 +57,9 @@ public class AllTheModium
         // Register the setup method for modloading
     	IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+	if(ModList.get().isLoaded("iceandfire")) {
+		ModEntityTypes.ENTITIES.register(modEventBus);
+	}
     	FluidList.FLUIDS.register(modEventBus);
        	FluidList.BLOCKS.register(modEventBus);
     	FluidList.ITEMS.register(modEventBus);
@@ -104,7 +107,6 @@ public class AllTheModium
 			if(ModList.get().isLoaded("iceandfire")) {
 				IAFForgeRecipes.init();
 				IAFForgeRecipes.regIaFItems(event);
-				ModEntityTypes.ENTITIES.register(modEventBus);
 			}
 		}
 	}
