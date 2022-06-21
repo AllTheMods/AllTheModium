d6eee9c71a869828c18dde11b5efc7960fe20c76
thevortex <james.g.bowley@gmail.com>
2022-06-21 09:30:51 -0600

    1.18.2 porting changes
    adds salvaging recipes for atm tools/gear, alterations to biomes/worldgen and feature registrations

--- src/main/java/com/thevortex/allthemodium/worldgen/ATMPlacedFeature.java
+++ src/main/java/com/thevortex/allthemodium/worldgen/ATMPlacedFeature.java
@@ -2,8 +2,6 @@ package com.thevortex.allthemodium.worldgen;
 
 import com.google.common.collect.ImmutableList;
 import com.thevortex.allthemodium.registry.ModRegistry;
-import net.allthemods.alltheores.infos.Configuration;
-import net.allthemods.alltheores.worldgen.ATOConfiguredFeature;
 import net.allthemods.alltheores.worldgen.features.ATOOtherFeatures;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.core.BlockPos;
@@ -58,15 +56,6 @@ public class ATMPlacedFeature {
 
 
 
-    public static final Holder<PlacedFeature> OTHER_ORE_COAL = PlacementUtils.register("allthemodium:ore_coal", ATOOtherFeatures.OTHER_ORE_COAL,commonOrePlacement(30, HeightRangePlacement.uniform(VerticalAnchor.absolute(136), VerticalAnchor.top())));
-    public static final Holder<PlacedFeature> OTHER_ORE_COPPER = PlacementUtils.register("allthemodium:ore_copper", ATOOtherFeatures.OTHER_ORE_COPPER,commonOrePlacement(16, HeightRangePlacement.triangle(VerticalAnchor.absolute(-16), VerticalAnchor.absolute(112))));
-    public static final Holder<PlacedFeature> OTHER_ORE_DIAMOND = PlacementUtils.register("allthemodium:ore_diamond", ATOOtherFeatures.OTHER_ORE_DIAMOND,commonOrePlacement(14, HeightRangePlacement.triangle(VerticalAnchor.absolute(-10), VerticalAnchor.absolute(129))));
-    public static final Holder<PlacedFeature> OTHER_ORE_EMERALD = PlacementUtils.register("allthemodium:ore_emerald", ATOOtherFeatures.OTHER_ORE_EMERALD,commonOrePlacement(20, HeightRangePlacement.triangle(VerticalAnchor.absolute(-16), VerticalAnchor.absolute(480))));
-    public static final Holder<PlacedFeature> OTHER_ORE_GOLD = PlacementUtils.register("allthemodium:ore_gold", ATOOtherFeatures.OTHER_ORE_GOLD,commonOrePlacement(20, PlacementUtils.RANGE_10_10));
-    public static final Holder<PlacedFeature> OTHER_ORE_IRON = PlacementUtils.register("allthemodium:ore_iron", ATOOtherFeatures.OTHER_ORE_IRON,commonOrePlacement(50, HeightRangePlacement.triangle(VerticalAnchor.absolute(-24), VerticalAnchor.absolute(384))));
-    public static final Holder<PlacedFeature> OTHER_ORE_LAPIS = PlacementUtils.register("allthemodium:ore_lapis", ATOOtherFeatures.OTHER_ORE_LAPIS,commonOrePlacement(4, HeightRangePlacement.uniform(VerticalAnchor.bottom(), VerticalAnchor.absolute(64))));
-    public static final Holder<PlacedFeature> OTHER_ORE_QUARTZ = PlacementUtils.register("allthemodium:ore_quartz", ATOOtherFeatures.OTHER_ORE_QUARTZ,commonOrePlacement(16, PlacementUtils.RANGE_10_10));
-    public static final Holder<PlacedFeature> OTHER_ORE_REDSTONE = PlacementUtils.register("allthemodium:ore_redstone", ATOOtherFeatures.OTHER_ORE_REDSTONE,commonOrePlacement(14, HeightRangePlacement.uniform(VerticalAnchor.bottom(), VerticalAnchor.absolute(15))));
 
 
 
---
+++ src/main/java/com/thevortex/allthemodium/worldgen/ATOtherPlacedFeatures.java
@@ -0,0 +1,30 @@
+package com.thevortex.allthemodium.worldgen;
+
+import net.allthemods.alltheores.worldgen.features.ATOOtherFeatures;
+import net.minecraft.core.Holder;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.world.level.levelgen.VerticalAnchor;
+import net.minecraft.world.level.levelgen.placement.*;
+
+import java.util.List;
+
+public class ATOtherPlacedFeatures {
+    public static final Holder<PlacedFeature> OTHER_ORE_COAL = PlacementUtils.register("allthemodium:ore_coal", ATOOtherFeatures.OTHER_ORE_COAL,commonOrePlacement(30, HeightRangePlacement.uniform(VerticalAnchor.absolute(136), VerticalAnchor.top())));
+    public static final Holder<PlacedFeature> OTHER_ORE_COPPER = PlacementUtils.register("allthemodium:ore_copper", ATOOtherFeatures.OTHER_ORE_COPPER,commonOrePlacement(16, HeightRangePlacement.triangle(VerticalAnchor.absolute(-16), VerticalAnchor.absolute(112))));
+    public static final Holder<PlacedFeature> OTHER_ORE_DIAMOND = PlacementUtils.register("allthemodium:ore_diamond", ATOOtherFeatures.OTHER_ORE_DIAMOND,commonOrePlacement(14, HeightRangePlacement.triangle(VerticalAnchor.absolute(-10), VerticalAnchor.absolute(129))));
+    public static final Holder<PlacedFeature> OTHER_ORE_EMERALD = PlacementUtils.register("allthemodium:ore_emerald", ATOOtherFeatures.OTHER_ORE_EMERALD,commonOrePlacement(20, HeightRangePlacement.triangle(VerticalAnchor.absolute(-16), VerticalAnchor.absolute(480))));
+    public static final Holder<PlacedFeature> OTHER_ORE_GOLD = PlacementUtils.register("allthemodium:ore_gold", ATOOtherFeatures.OTHER_ORE_GOLD,commonOrePlacement(20, PlacementUtils.RANGE_10_10));
+    public static final Holder<PlacedFeature> OTHER_ORE_IRON = PlacementUtils.register("allthemodium:ore_iron", ATOOtherFeatures.OTHER_ORE_IRON,commonOrePlacement(50, HeightRangePlacement.triangle(VerticalAnchor.absolute(-24), VerticalAnchor.absolute(384))));
+    public static final Holder<PlacedFeature> OTHER_ORE_LAPIS = PlacementUtils.register("allthemodium:ore_lapis", ATOOtherFeatures.OTHER_ORE_LAPIS,commonOrePlacement(4, HeightRangePlacement.uniform(VerticalAnchor.bottom(), VerticalAnchor.absolute(64))));
+    public static final Holder<PlacedFeature> OTHER_ORE_QUARTZ = PlacementUtils.register("allthemodium:ore_quartz", ATOOtherFeatures.OTHER_ORE_QUARTZ,commonOrePlacement(16, PlacementUtils.RANGE_10_10));
+    public static final Holder<PlacedFeature> OTHER_ORE_REDSTONE = PlacementUtils.register("allthemodium:ore_redstone", ATOOtherFeatures.OTHER_ORE_REDSTONE,commonOrePlacement(14, HeightRangePlacement.uniform(VerticalAnchor.bottom(), VerticalAnchor.absolute(15))));
+
+
+    private static List<PlacementModifier> orePlacement(PlacementModifier placement, PlacementModifier placement2) {
+        return List.of(placement, InSquarePlacement.spread(), placement2, BiomeFilter.biome());
+
+    }
+    private static List<PlacementModifier> commonOrePlacement(int count, PlacementModifier pm) {
+        return orePlacement(CountPlacement.of(count), pm);
+    }
+}
