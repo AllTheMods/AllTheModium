d1ef760f7c13c68b7a4156a5c247df6e265e68ec
jlwoolf <jlwoolf@proton.me>
2023-10-13 10:57:18 -0600

    added config classes

---
+++ src/main/java/com/thevortex/allthemodium/config/AllTheModiumClientConfigs.java
@@ -0,0 +1,15 @@
+package com.thevortex.allthemodium.config;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public class AllTheModiumClientConfigs {
+    public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+    public static final ForgeConfigSpec SPEC;
+
+    static {
+        BUILDER.push("AllTheModium Configs");
+
+        BUILDER.pop();
+        SPEC = BUILDER.build();
+    }
+}
---
+++ src/main/java/com/thevortex/allthemodium/config/AllTheModiumCommonConfigs.java
@@ -0,0 +1,32 @@
+package com.thevortex.allthemodium.config;
+
+import java.util.List;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public class AllTheModiumCommonConfigs {
+    public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+    public static final ForgeConfigSpec SPEC;
+
+    public static final ForgeConfigSpec.ConfigValue<Boolean> ALLTHEMODIUM_QUARRYABLE;
+    public static final ForgeConfigSpec.ConfigValue<Boolean> UNOBTAINIUM_QUARRYABLE;
+    public static final ForgeConfigSpec.ConfigValue<Boolean> VIBRANIUM_QUARRYABLE;
+
+    //public static final ForgeConfigSpec.ConfigValue<List<String>> ALLTHEMODIUM_BIOMES;
+
+    static {
+        BUILDER.push("AllTheModium Configs");
+        ALLTHEMODIUM_QUARRYABLE = BUILDER
+                .comment("Whether or not Allthemodium Ore should be quarryable or only player mineable")
+                .define("Allthemodium Quarryable", false);
+        UNOBTAINIUM_QUARRYABLE = BUILDER
+                .comment("Whether or not Unobtainium Ore should be quarryable or only player mineable")
+                .define("Unobtainium Quarryable", false);
+        VIBRANIUM_QUARRYABLE = BUILDER
+                .comment("Whether or not Vibranium Ore should be quarryable or only player mineable")
+                .define("Vibranium Quarryable", false);
+
+        BUILDER.pop();
+        SPEC = BUILDER.build();
+    }
+}
