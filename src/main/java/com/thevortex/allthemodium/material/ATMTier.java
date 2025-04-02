24e1da44229b6fcab5b35bbae664419084f1e2be
thevortex <james.g.bowley@gmail.com>
2025-04-01 18:23:02 -0600

    change tier registry

--- src/main/java/com/thevortex/allthemodium/material/ATMTier.java
+++ src/main/java/com/thevortex/allthemodium/material/ATMTier.java
@@ -20,7 +20,7 @@ public class ATMTier implements Tier {
     public static final ATMTier ALLTHEMODIUM = createTier(TagRegistry.INCORRECT_FOR_ALLTHEMODIUM_TOOL, 15.0F, 9001,  12.0F, 85, Ingredient.of(TagRegistry.ALLTHEMODIUM_INGOT));
     public static final ATMTier VIBRANIUM = createTier(TagRegistry.INCORRECT_FOR_VIBRANIUM_TOOL, 16.0F, 9001,  25.0F, 100, Ingredient.of(TagRegistry.VIBRANIUM_INGOT));
     public static final ATMTier UNOBTAINIUM = createTier(TagRegistry.INCORRECT_FOR_UNOBTAINIUM_TOOL, 18.0F, 9001,  35.0F, 125, Ingredient.of(TagRegistry.UNOBTAINIUM_INGOT));
-    public static final ATMTier ALLOY = createTier(TagRegistry.INCORRECT_FOR_ALLOY_TOOL, 35.0F, 9001,  65.0F, 200, Ingredient.of(ModRegistry.UNOBTAINIUM_ALLTHEMODIUM_ALLOY.get()));
+    public static final ATMTier ALLOY = createTier(TagRegistry.INCORRECT_FOR_ALLOY_TOOL, 35.0F, 9001,  65.0F, 200, Ingredient.of(ModRegistry.UNOBTAINIUM_ALLTHEMODIUM_ALLOY::get));
     public ATMTier(TagKey<Block> incorrectBlocksForDrops, float speed, int uses, float attackBonus, int enchantmentValue, Ingredient repairIngredient) {
         this.attackBonus = attackBonus;
         this.enchantmentValue = enchantmentValue;
